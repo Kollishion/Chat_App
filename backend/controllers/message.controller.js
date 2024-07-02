c88ea6a821e54b3f61a004ba7c0aeb0a13d674e0
Abhishek <abhishek321bose@gmail.com>
2024-07-02 19:49:26 +0530

    messageController.js completed with routes without socketIO functionality for now

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -41,8 +41,16 @@ export const sendMessage = async (req, res) => {
 
 export const getMessages = async (req, res) => {
   try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    res.status(200).json(conversation.messages);
   } catch (error) {
-    console.log("Error in sendMessage:", error.message);
+    console.log("Error in getMessage:", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
