01be522a57b256abf242b5c5aa80c9b1072cb835
Abhishek <abhishek321bose@gmail.com>
2024-08-10 18:12:44 +0530

    did the message container again for when the user doesn't select any chat to talk to andd diplay a get started by chatting message on right

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -25,13 +25,13 @@ export default MessageInput;
 // const MessageInput = () => {
 //   return (
 //     <form className="px-4 my-3">
-//       <div className="w-full">
+//       <div className="w-full relative">
 //         <input
 //           type="text"
 //           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
 //           placeholder="Message"
 //         />
-//         <button className="absolute lg:top-[66.5vh] lg:right-4 sm:top-[53vh] sm:right-3 inset-y-0 end-0 flex items-center pe-3">
+//         <button className="absolute inset-y-0 end-0 flex items-center pe-3">
 //           <BsSend />
 //         </button>
 //       </div>
